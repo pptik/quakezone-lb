a00b2971454a9c4c884a5a2da8a5ea50db83c7db
Hendy Irawan <hendy@hendyirawan.com>
2019-09-18 20:46:45 +0700

    import

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import {QuakeZone} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {QuakeZone};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new QuakeZone(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
